5ea2d38fa02ce96400a31f1ae73100dc37ffbedc
yoes monterroza <yoesmonterroza@hotmail.com>
2024-04-14 20:42:36 -0500

    entities ElectoralJourney and ElectoralPosition added

---
+++ Elections/Elections.Shared/Entities/ElectoralJourney.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Elections.Shared.Entities
+{
+    public class ElectoralJourney
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Jornada Electoral")]
+        [MaxLength(80, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        public string Name { get; set; } = null!;
+
+        [Display(Name = "Fecha Jornada")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        public DateTime? Date { get; set; }
+    }
+}
---
+++ Elections/Elections.Shared/Entities/ElectoralPosition.cs
@@ -0,0 +1,15 @@
+﻿using Elections.Shared.Interfaces;
+using System.ComponentModel.DataAnnotations;
+
+namespace Elections.Shared.Entities
+{
+    public class ElectoralPosition : IEntityWithName
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Cargo Electoral")]
+        [MaxLength(50, ErrorMessage ="El campo {0} no puede tener mas de {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        public string Name { get; set; } = null!;
+    }
+}
