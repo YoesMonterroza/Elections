978e48f81af8de828789cd49df7a682f94230ae9
Juan Parada <juanpape24@gmail.com>
2024-04-18 00:32:34 -0500

    Add VotingStation page

---
+++ Elections/Elections.Frontend/Shared/GenericList.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Elections.Frontend.Shared
+{
+    public partial class GenericList<Titem>
+    {
+        [EditorRequired]
+        [Parameter]
+        public RenderFragment Body { get; set; } = null!;
+
+        [Parameter]
+        public RenderFragment? Loading { get; set; }
+
+        [EditorRequired, Parameter] public List<Titem> MyList { get; set; } = null!;
+        [Parameter]
+        public RenderFragment? NoRecords { get; set; }
+    }
+}
