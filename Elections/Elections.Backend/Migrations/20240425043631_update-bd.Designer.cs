cde135f3895bc9223b811c90b5acf6da9a38b24b
Juan Parada <juanpape24@gmail.com>
2024-04-24 23:40:16 -0500

    update database

--- Elections/Elections.Backend/Migrations/20240425043631_update-bd.Designer.cs
+++ Elections/Elections.Backend/Migrations/20240425043631_update-bd.Designer.cs
@@ -4,6 +4,7 @@ using Elections.Backend.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Elections.Backend.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20240425043631_update-bd")]
+    partial class updatebd
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
