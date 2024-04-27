2a3952088bae62cd295fb957d158feb74119e24b
yoes monterroza <yoesmonterroza@hotmail.com>
2024-04-27 03:18:10 -0500

    Create and edit ElectoralJourney added

---
+++ Elections/Elections.Frontend/Pages/ElectoralJourneys/ElectoralJourneyEdit.razor.cs
@@ -0,0 +1,65 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Elections.Frontend.Pages.ElectoralPositions;
+using Elections.Frontend.Repositories;
+using Elections.Shared.Entities;
+using Microsoft.AspNetCore.Components;
+
+namespace Elections.Frontend.Pages.ElectoralJourneys
+{
+    public partial class ElectoralJourneyEdit
+    {
+        private ElectoralJourney? electoralJourney;
+        private ElectoralJourneyForm? electoralJourneyForm;
+        [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+        [Inject] private IRepository Repository { get; set; } = null!;
+        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+        [Parameter] public int Id { get; set; }
+
+        private readonly String VOTING_STATION_PATH = "api/electoralJourneys";
+        protected override async Task OnInitializedAsync()
+        {
+            var responseHttp = await Repository.GetAsync<ElectoralJourney>(string.Concat(VOTING_STATION_PATH, $"/{Id}"));
+            if (responseHttp.Error)
+            {
+                if (responseHttp.HttpResponseMessage.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    NavigationManager.NavigateTo("electoralJourneys");
+                }
+                else
+                {
+                    var messageError = await responseHttp.GetErrorMessageAsync();
+                    await SweetAlertService.FireAsync("Error", messageError, SweetAlertIcon.Error);
+                }
+            }
+            else
+            {
+                electoralJourney = responseHttp.Response;
+            }
+        }
+        private async Task EditAsync()
+        {
+            var responseHttp = await Repository.PutAsync(VOTING_STATION_PATH, electoralJourney);
+            if (responseHttp.Error)
+            {
+                var mensajeError = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", mensajeError, SweetAlertIcon.Error);
+                return;
+            }
+            Return();
+            var toast = SweetAlertService.Mixin(new SweetAlertOptions
+            {
+                Toast = true,
+                Position = SweetAlertPosition.BottomEnd,
+                ShowConfirmButton = true,
+                Timer = 3000
+            });
+            await toast.FireAsync(icon: SweetAlertIcon.Success, message: "Cambios guardados con éxito.");
+        }
+        private void Return()
+        {
+            electoralJourneyForm!.FormPostedSuccessfully = true;
+            NavigationManager.NavigateTo("electoralJourneys");
+        }
+
+        }
+    }
---
+++ Elections/Elections.Frontend/Pages/ElectoralJourneys/ElectoralJourneyForm.razor.cs
@@ -0,0 +1,49 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Elections.Shared.Entities;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+
+namespace Elections.Frontend.Pages.ElectoralJourneys
+{
+    public partial class ElectoralJourneyForm
+    {
+        private EditContext editContext = null!;
+        public bool FormPostedSuccessfully { get; set; } = false;
+
+        [EditorRequired, Parameter] public EventCallback OnValidSubmit { get; set; }
+
+        [EditorRequired, Parameter] public EventCallback ReturnAction { get; set; }
+
+        [EditorRequired, Parameter] public ElectoralJourney ElectoralJourney { get; set; } = null!;
+
+        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+
+        protected override void OnInitialized()
+        {
+            editContext = new(ElectoralJourney);
+        }
+        private async Task OnBeforeInternalNavigation(LocationChangingContext context)
+        {
+            var formWasEdited = editContext.IsModified();
+            if (!formWasEdited || FormPostedSuccessfully)
+            {
+                return;
+            }
+            var result = await SweetAlertService.FireAsync(new SweetAlertOptions
+            {
+                Title = "Confirmación",
+                Text = "¿Deseas abandonar la página y perder los cambios?",
+                Icon = SweetAlertIcon.Warning,
+                ShowCancelButton = true
+            });
+            var confirm = !string.IsNullOrEmpty(result.Value);
+            if (confirm)
+            {
+                return;
+            }
+            context.PreventNavigation();
+        }
+
+    }
+}
