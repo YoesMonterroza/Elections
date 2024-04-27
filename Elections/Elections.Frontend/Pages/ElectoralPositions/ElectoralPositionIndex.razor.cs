b74a58df6687767cd3ee2aced3e7de935534d15f
yoes monterroza <yoesmonterroza@hotmail.com>
2024-04-27 01:48:08 -0500

    ElectoralPositionIndex.razor added

---
+++ Elections/Elections.Frontend/Pages/ElectoralPositions/ElectoralPositionIndex.razor.cs
@@ -0,0 +1,28 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Elections.Frontend.Repositories;
+using Elections.Shared.Entities;
+using Microsoft.AspNetCore.Components;
+
+namespace Elections.Frontend.Pages.ElectoralPositions
+{
+    public partial class ElectoralPositionIndex
+    {
+        [Inject] private IRepository Repository { get; set; } = null!;
+        public List<ElectoralPosition>? ElectoralPositions { get; set; }
+        private int currentPage = 1;
+        private int totalPages;
+
+        [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+
+        [Parameter, SupplyParameterFromQuery] public string Page { get; set; } = string.Empty;
+        [Parameter, SupplyParameterFromQuery] public string Filter { get; set; } = string.Empty;
+        [Parameter, SupplyParameterFromQuery] public int RecordsNumber { get; set; } = 10;
+        protected async override Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            var responseHttp = await Repository.GetAsync<List<ElectoralPosition>>("api/electoralPositions");
+            ElectoralPositions = responseHttp.Response;
+        }
+    }
+}
