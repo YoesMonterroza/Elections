40a51e2ea41de699fa0a0c70944a7a50dcac6a71
yoes monterroza <yoesmonterroza@hotmail.com>
2024-04-27 23:01:01 -0500

    Controller of ElectoralJourney and End Electoral Position update with UnitOfWork

--- Elections/Elections.Backend/Controllers/ElectoralJourneysController.cs
+++ Elections/Elections.Backend/Controllers/ElectoralJourneysController.cs
@@ -1,5 +1,7 @@
 ﻿using Elections.Backend.Data;
+using Elections.Backend.UnitsOfWork.Implementations;
 using Elections.Backend.UnitsOfWork.Interfaces;
+using Elections.Shared.DTOs;
 using Elections.Shared.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,8 +12,55 @@ namespace Elections.Backend.Controllers
     [Route("api/[controller]")]
     public class ElectoralJourneysController : GenericController<ElectoralJourney>
     {
-        public ElectoralJourneysController(IGenericUnitOfWork<ElectoralJourney> unitOfWork) : base(unitOfWork)
+        private readonly IElectoralJourneysUnitOfWork _electoralJourneysUnitOfWork;
+        public ElectoralJourneysController(IGenericUnitOfWork<ElectoralJourney> unitOfWork, IElectoralJourneysUnitOfWork electoralJourneysUnitOfWork) : base(unitOfWork)
         {
+            _electoralJourneysUnitOfWork = electoralJourneysUnitOfWork;
         }
+
+        [HttpGet("full")]
+        public override async Task<IActionResult> GetAsync()
+        {
+            var response = await _electoralJourneysUnitOfWork.GetAsync();
+            if (response.WasSuccess)
+            {
+                return Ok(response.Result);
+            }
+            return BadRequest();
+        }
+
+        [HttpGet("{id}")]
+        public override async Task<IActionResult> GetAsync(int id)
+        {
+            var response = await _electoralJourneysUnitOfWork.GetAsync(id);
+            if (response.WasSuccess)
+            {
+                return Ok(response.Result);
+            }
+            return NotFound(response.Message);
+        }
+
+        [HttpGet]
+        public override async Task<IActionResult> GetAsync(PaginationDTO pagination)
+        {
+            var response = await _electoralJourneysUnitOfWork.GetAsync(pagination);
+            if (response.WasSuccess)
+            {
+                return Ok(response.Result);
+            }
+            return BadRequest();
+        }
+
+        [HttpGet("totalPages")]
+        public override async Task<IActionResult> GetPagesAsync([FromQuery] PaginationDTO pagination)
+        {
+            var action = await _electoralJourneysUnitOfWork.GetTotalPagesAsync(pagination);
+            if (action.WasSuccess)
+            {
+                return Ok(action.Result);
+            }
+            return BadRequest();
+        }
+
     }
 }
--- Elections/Elections.Backend/Controllers/ElectoralPositionsController.cs
+++ Elections/Elections.Backend/Controllers/ElectoralPositionsController.cs
@@ -1,5 +1,7 @@
 ﻿using Elections.Backend.Data;
+using Elections.Backend.UnitsOfWork.Implementations;
 using Elections.Backend.UnitsOfWork.Interfaces;
+using Elections.Shared.DTOs;
 using Elections.Shared.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,8 +12,54 @@ namespace Elections.Backend.Controllers
     [Route("api/[controller]")]
     public class ElectoralPositionsController : GenericController<ElectoralPosition>
     {
-        public ElectoralPositionsController(IGenericUnitOfWork<ElectoralPosition> unitOfWork) : base(unitOfWork)
+        private readonly IElectoralPositionsUnitOfWork _electoralPositionsUnitOfWork;
+        public ElectoralPositionsController(IGenericUnitOfWork<ElectoralPosition> unitOfWork, IElectoralPositionsUnitOfWork electoralPositionsUnitOfWork) : base(unitOfWork)
         {
+            _electoralPositionsUnitOfWork = electoralPositionsUnitOfWork;
         }
+        [HttpGet("full")]
+        public override async Task<IActionResult> GetAsync()
+        {
+            var response = await _electoralPositionsUnitOfWork.GetAsync();
+            if (response.WasSuccess)
+            {
+                return Ok(response.Result);
+            }
+            return BadRequest();
+        }
+
+        [HttpGet("{id}")]
+        public override async Task<IActionResult> GetAsync(int id)
+        {
+            var response = await _electoralPositionsUnitOfWork.GetAsync(id);
+            if (response.WasSuccess)
+            {
+                return Ok(response.Result);
+            }
+            return NotFound(response.Message);
+        }
+
+        [HttpGet]
+        public override async Task<IActionResult> GetAsync(PaginationDTO pagination)
+        {
+            var response = await _electoralPositionsUnitOfWork.GetAsync(pagination);
+            if (response.WasSuccess)
+            {
+                return Ok(response.Result);
+            }
+            return BadRequest();
+        }
+
+        [HttpGet("totalPages")]
+        public override async Task<IActionResult> GetPagesAsync([FromQuery] PaginationDTO pagination)
+        {
+            var action = await _electoralPositionsUnitOfWork.GetTotalPagesAsync(pagination);
+            if (action.WasSuccess)
+            {
+                return Ok(action.Result);
+            }
+            return BadRequest();
+        }
+
     }
 }
