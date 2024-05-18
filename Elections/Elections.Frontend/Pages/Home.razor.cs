57f441b0530e36d5b360e9923c60d21ced32eb56
Juan Parada <juanpape24@gmail.com>
2024-05-18 02:09:25 -0500

    Se ajusta home y se agrega carousel

---
+++ Elections/Elections.Frontend/Pages/Home.razor.cs
@@ -0,0 +1,15 @@
+﻿namespace Elections.Frontend.Pages
+{
+    public partial class Home
+    {
+        private List<string>? adminImages;
+        private List<string>? anonymImages;
+        private List<string>? userImages;
+        protected override async Task OnInitializedAsync()
+        {
+            adminImages = ["resources/1.png", "resources/2_Admin.png", "resources/3.png"];
+            userImages = ["resources/1.png", "resources/2_user.png", "resources/3.png"];
+            anonymImages = ["resources/1.png", "resources/2_Anonimo.png", "resources/3_Anonimo.png"];
+        }
+    }
+}
