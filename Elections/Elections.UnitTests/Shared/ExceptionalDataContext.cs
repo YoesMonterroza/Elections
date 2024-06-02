8443cb7fbb1a6409f50bd03a3a9e7db81c8f662e
yoes monterroza <yoesmonterroza@hotmail.com>
2024-06-02 17:37:16 -0500

    Shared for test added

---
+++ Elections/Elections.UnitTests/Shared/ExceptionalDataContext.cs
@@ -0,0 +1,22 @@
+﻿using Elections.Backend.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elections.UnitTests.Shared
+{
+    public class ExceptionalDataContext : DataContext
+    {
+        public ExceptionalDataContext(DbContextOptions<DataContext> options)
+:       base(options)
+        { }
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            throw new InvalidOperationException("Test Exception");
+        }
+
+    }
+}
