90a3bb68e149619100fb4df807d60d03141813b3
Juan Parada <juanpape24@gmail.com>
2024-06-04 22:51:15 -0500

    add page of results

---
+++ Elections/Elections.Frontend/Pages/Results/Results.razor.cs
@@ -0,0 +1,93 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Elections.Frontend.Repositories;
+using Elections.Shared.Entities;
+using Microsoft.AspNetCore.Components;
+
+namespace Elections.Frontend.Pages.Results
+{
+    public partial class Results
+    {
+        [Inject] private IRepository Repository { get; set; } = null!;
+        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+        public List<ElectoralJourney>? ElectoralJourneys { get; set; }
+
+        private List<City> cities = new();
+        private List<State> states = new();
+        private List<Country> countries = new();
+        private int countryselected;
+        private int stateselected;
+        private int cityselected;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadCountriesAsync();
+        }
+
+        private async Task LoadCountriesAsync()
+        {
+            var responseHttp = await Repository.GetAsync<List<Country>>("/api/countries/combo");
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+
+            countries = responseHttp.Response;
+        }
+
+        private async Task CountryChangedAsync(ChangeEventArgs e)
+        {
+            var selectedCountry = Convert.ToInt32(e.Value!);
+            states = null;
+            cities = null;
+            await LoadStatesAsync(selectedCountry);
+        }
+
+        private async Task LoadStatesAsync(int countryId)
+        {
+            var responseHttp = await Repository.GetAsync<List<State>>($"/api/states/combo/{countryId}");
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+
+            states = responseHttp.Response;
+        }
+
+        private async Task StateChangedAsync(ChangeEventArgs e)
+        {
+            var selectedState = Convert.ToInt32(e.Value!);
+            cities = null;
+            await LoadCitiesAsync(selectedState);
+        }
+
+        private async Task LoadCitiesAsync(int stateId)
+        {
+            var responseHttp = await Repository.GetAsync<List<City>>($"/api/cities/combo/{stateId}");
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+
+            cities = responseHttp.Response;
+        }
+
+        private async Task LoadElectoralJourneysAsync()
+        {
+            var responseHttp = await Repository.GetAsync<List<City>>($"/api/cities/combo/{cityselected}");
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+
+            cities = responseHttp.Response;
+        }
+    }
+}
